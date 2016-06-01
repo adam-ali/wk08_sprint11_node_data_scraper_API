122e452f811281b295b508543a8172981d7ca7cd
adam-ali <adam_ali_96@hotmail.co.uk>
2016-06-01 10:43:59 +0100

    api now working

--- server.js
+++ server.js
@@ -3,6 +3,40 @@ var app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var server = express.Router();
+var Schema = mongoose.Schema;
+
+app.use('/api',server);
+mongoose.connect('mongodb://localhost/CWLeaderBoard');
+
+server.param('userName',function (req,res,next,userName) {
+    User.findOne({user: userName},function(err,doc){
+        if (err) return  res.sendStatus(404);
+        req.dev= doc;
+        next();
+    });
+});
+
+var CWLeaderBoard = new Schema({
+    user: String,
+    honor: Number,
+    position: Number,
+    rank: Number,
+    profileUrl: String
+});
+var User = mongoose.model('User', CWLeaderBoard);
+
+server.route('/')
+    .get(function (req, res) {
+        User.find({}, function(err,doc){
+            return res.status(200).json(doc);
+        });
+    });
+
+server.route('/:userName')
+    .get(function (req,res) { //get developer by userName
+        res.json(req.dev);
+    });
 
 
-mongoose.connect('mongodb://localhost:27017/CWLeaderBoard')
+app.listen(3001);
